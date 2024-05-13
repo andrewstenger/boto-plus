6aef021b705c6280ac6a04f560e77e8240e20445
andrewstenger <andrew.stenger93@gmail.com>
2024-05-13 14:59:36 -0400

    (fix) removed redundant dependencies from setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setuptools.setup(
    name='boto_plus',
    version='1.0',
    description='',
-   author='',
+   author='Andrew Stenger',
    author_email='',
-   #packages=['boto_plus'],  #same as name
    packages=setuptools.find_packages(),
-   install_requires=["setuptools >= 61.0", "pandas", "boto3"], #external packages as dependencies
 )
